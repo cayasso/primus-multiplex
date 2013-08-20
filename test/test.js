f879e96473f27ea3c40be96f55ac1c75a6a33e70
Jonathan Brumley <cayasso@gmail.com>
2013-08-19 19:17:33 -0600

    Adding tests for emitter with rooms

--- test/test.js
+++ test/test.js
@@ -580,6 +580,88 @@ describe('primus-multiplex', function (){
 
     });
 
+    it('should allow broadcasting a message to a client with emitter', function(done){
+      var srv = http();
+      var primus = server(srv, opts);
+      var a = primus.channel('a');
+      srv.listen(function(){
+        a.on('connection', function (spark) {
+          spark.on('join', function (room) {
+            spark.join(room);
+          });
+
+          spark.on('msg', function (msg) {
+            if ('broadcast' === msg) {
+              spark.room('r1').emit('msg', 'hi');
+            }
+          });
+        });
+      });
+      var cl = client(srv, primus);
+      var c1a = cl.channel('a');
+      c1a.on('msg', function (msg) {
+        expect(msg).to.be('hi');
+        done();
+      });
+      c1a.emit('join', 'r1');
+      setTimeout(function () {
+        var me = cl.channel('a');
+        me.emit('msg', 'broadcast');
+      }, 0);
+
+    });
+
+    it('should allow broadcasting a message to multiple clients with emitter', function(done){
+      var srv = http();
+      var primus = server(srv, opts);
+      var a = primus.channel('a');
+      var count = 3;
+
+      srv.listen(function(){
+        a.on('connection', function (spark) {
+          spark.on('join', function (room) {
+            spark.join(room);
+          });
+
+          spark.on('msg', function (msg) {
+            if ('broadcast' === msg) {
+              spark.room('r1 r2 r3').emit('msg', 'hi');
+            }
+          });
+        });
+      });
+
+      var cl = client(srv, primus);
+      var c1a = cl.channel('a');
+      var c2a = cl.channel('a');
+      var c3a = cl.channel('a');
+
+      c1a.emit('join', 'r1');
+      c2a.emit('join', 'r2');
+      c3a.emit('join', 'r3');
+
+      c1a.on('msg', function (msg) {
+        expect(msg).to.be('hi');
+        if (!--count) done();
+      });
+
+      c2a.on('msg', function (msg) {
+        expect(msg).to.be('hi');
+        if (!--count) done();
+      });
+
+      c3a.on('msg', function (msg) {
+        expect(msg).to.be('hi');
+        if (!--count) done();
+      });
+
+      setTimeout(function () {
+        var me = cl.channel('a');
+        me.emit('msg', 'broadcast');
+      }, 0);
+
+    });
+
   });
 
 });
